726c3d032a1df5fa5cacbb09dd9531a798bbcb38
James Youn <jamesyoun710@gmail.com>
2016-07-15 11:07:13 -0700

    Init commit

---
+++ anagram.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+function get_line(filename, line_no, callback) {
+  fs.readFile(filename, function (err, data) {
+    if (err) throw err;
+
+    // Data is a buffer that we need to convert to a string
+    // Improvement: loop over the buffer and stop when the line is reached
+    var lines = data.toString('utf-8').split("\n");
+
+    if(+line_no > lines.length){
+      return callback('File end reached without finding line', null);
+    }
+
+    callback(null, lines[+line_no]);
+  });
+}
+
+get_line('./Word_List.txt', 9, function(err, line){
+  console.log('The line: ' + line);
+})
+
